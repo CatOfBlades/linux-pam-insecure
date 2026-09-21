76030e51ba9addba88ae2222850b26eadd3ca38d
CatOfBlades <Fergley@yahoo.com>
2026-09-21 11:19:30 +0000

    Impliment xor.c

--- modules/pam_unix/xor.c
+++ modules/pam_unix/xor.c
@@ -1,15 +1,29 @@
-// TODO: impliment an xor encryption algorithm as the seccond worst idea to hash paswords since plaintext
+// Impliments an xor encryption algorithm, the seccond worst idea to hash paswords since plaintext
+#include "xor.h"
+#include <string.h>
 
-char *xor_create(const char *password);
+#define XOR_KEY "\xb0\x0b\x1e\x22" //heh "boobiess"
+
+char *xor_create(const char *password)
+{
+    const char *key = XOR_KEY;
+    size_t str_len = strlen(password);
+    size_t key_len = strlen(key);
+    char buffer[2048];
+    
+    for (size_t i = 0; i < str_len; i++)
+    {
+        buffer[i] ^= key[i % key_len];  // Cycle through key bytes
+        buffer[i+1] = 0x00; //keep the end of the string a zero to match c-style string convention.
+    }
+    return buffer;
+}
 
 int xor_verify(const char *password, const char *stored)
 {
-  // if ( xor_create(password) == stored ) // this should use strcmp or something when actually implimented
-  // {
-  //   return 1; // 1 or whatever success is
-  // }
-  // else
-  // {
-  //   return 0; // our failure case
-  // }
+    char *computed = xor_create(password);
+    if (!computed) return 0;  // creation failed
+  
+    int match = pam_consttime_streq(computed, stored);
+    return match;
 }
