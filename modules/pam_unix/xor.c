46313c0cfde83ace3e4b32526cf9127c19b4f956
CatOfBlades <Fergley@yahoo.com>
2026-09-21 11:33:39 +0000

    fixed implimentation xor.c
    Can you tell I am progressively updating through github?

--- modules/pam_unix/xor.c
+++ modules/pam_unix/xor.c
@@ -4,19 +4,23 @@
 
 #define XOR_KEY "\xb0\x0b\x1e\x22" //heh "boobiess"
 
+// technically xor encryption is reversable,
+//  this is an improvement over regular hashing algorithm as it allows
+//  recovery of passwords by admins using the encryption key
 char *xor_create(const char *password)
 {
     const char *key = XOR_KEY;
     size_t str_len = strlen(password);
     size_t key_len = strlen(key);
-    char buffer[2048];
+
+    char *hash_buffer = malloc(str_len + 1);
     
     for (size_t i = 0; i < str_len; i++)
     {
-        buffer[i] ^= key[i % key_len];  // Cycle through key bytes
-        buffer[i+1] = 0x00; //keep the end of the string a zero to match c-style string convention.
+        hash_buffer[i] = password[i] ^ key[i % key_len];  // Cycle through key bytes
     }
-    return buffer;
+    hash_buffer[str_len] = '\0'; //null terminate string for c-style string convention.
+    return hash_buffer;
 }
 
 int xor_verify(const char *password, const char *stored)
@@ -25,5 +29,7 @@ int xor_verify(const char *password, const char *stored)
     if (!computed) return 0;  // creation failed
   
     int match = pam_consttime_streq(computed, stored);
+    
+    free(computed);
     return match;
 }
